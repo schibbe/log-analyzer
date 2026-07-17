a6434a61f25e83ab5cdc06dc3d39e7094c25a665
Simon Grzybowski <beertothebones@yahoo.de>
2026-07-17 17:45:09 +0200

    Count failed SSH login attempts
    Added a counter for failed SSH login attempts.

--- src/log-analyzer.py
+++ src/log-analyzer.py
@@ -1,7 +1,11 @@
 log_data = open("data/auth.log", "r")
 
+failed_login_count = 0
+
 for line in log_data:
     if "Failed password" in line:
-        print(line)
+        failed_login_count = failed_login_count + 1
+
+print(failed_login_count)
 
 log_data.close()
