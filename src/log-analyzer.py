05d85ba2bc014c40ec0c0de20458c78ccc8305c5
Simon Grzybowski <beertothebones@yahoo.de>
2026-07-17 18:27:18 +0200

    Store attacker IP addresses
    Collect source IP addresses from failed SSH login attempts and store them in a list for later analysis.

--- src/log-analyzer.py
+++ src/log-analyzer.py
@@ -1,11 +1,17 @@
 log_data = open("data/auth.log", "r")
 
 failed_login_count = 0
+failed_ips = []
 
 for line in log_data:
     if "Failed password" in line:
         failed_login_count = failed_login_count + 1
 
-print(failed_login_count)
+        ip = line.split("from ")[1].split(" ")[0]
+        failed_ips.append(ip)
+
+print(f"Failed login attempts: {failed_login_count}")
+print("Attacker IPs:")
+print(failed_ips)
 
 log_data.close()
