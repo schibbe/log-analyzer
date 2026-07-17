42d4ea198a93144ecc38d55cf7ab54ee3e95c1c5
Simon Grzybowski <beertothebones@yahoo.de>
2026-07-17 23:18:11 +0200

    Count targeted user accounts
    Track targeted SSH user accounts

--- src/log-analyzer.py
+++ src/log-analyzer.py
@@ -2,17 +2,24 @@ log_data = open("data/auth.log", "r")
 
 failed_login_count = 0
 ip_counts = {}
+user_counts = {}
 
 for line in log_data:
     if "Failed password" in line:
         failed_login_count = failed_login_count + 1
 
         ip = line.split("from ")[1].split(" ")[0]
+        user = line.split("for ")[1].split("from ")[0].strip()
 
         if ip in ip_counts:
             ip_counts[ip] = ip_counts[ip] + 1
         else:
             ip_counts[ip] = 1
+
+        if user in user_counts:
+            user_counts[user] = user_counts[user] + 1
+        else:
+            user_counts[user] = 1
 
 print(f"Failed login attempts: {failed_login_count}")
 print()
@@ -22,5 +29,12 @@ print("-------------------")
 
 for ip in ip_counts:
     print(f"{ip} -> {ip_counts[ip]} attempts")
+
+print()
+print("Target Accounts")
+print("---------------")
+
+for user in user_counts:
+    print(f"{user} -> {user_counts[user]} attempts")
 
 log_data.close()
