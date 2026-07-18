c039c7fb526f0d3d07f8fc8bf569fcf54320811c
Simon Grzybowski <beertothebones@yahoo.de>
2026-07-19 01:51:48 +0200

    Track successful user logins
    Detect successful SSH logins, count successful authentication events, and track successful user accounts.

--- src/log-analyzer.py
+++ src/log-analyzer.py
@@ -1,12 +1,17 @@
 log_data = open("data/auth.log", "r")
 
 failed_login_count = 0
+successful_login_count = 0
+
 ip_counts = {}
 user_counts = {}
+successful_users = {}
 ip_users = {}
 
 for line in log_data:
+
     if "Failed password" in line:
+
         failed_login_count += 1
 
         ip = line.split("from ")[1].split(" ")[0]
@@ -30,7 +35,22 @@ for line in log_data:
         else:
             ip_users[ip][user] = 1
 
+    if "Accepted password" in line:
+
+        successful_login_count += 1
+
+        user = line.split("for ")[1].split("from ")[0].strip()
+
+        if user in successful_users:
+            successful_users[user] += 1
+        else:
+            successful_users[user] = 1
+
+print("Authentication Summary")
+print("----------------------")
 print(f"Failed login attempts: {failed_login_count}")
+print(f"Successful logins: {successful_login_count}")
+
 print()
 
 sorted_ips = sorted(
@@ -61,10 +81,19 @@ for user, count in sorted_users:
 
 print()
 
+print("Successful Users")
+print("----------------")
+
+for user, count in successful_users.items():
+    print(f"{user} -> {count} successful logins")
+
+print()
+
 print("Attack Relationships")
 print("--------------------")
 
 for ip in ip_users:
+
     print(ip)
 
     for user, count in ip_users[ip].items():
